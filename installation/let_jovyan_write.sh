e3e6ce90cd3480070a594e9e53fff4c814d8061a
vadikch <v.v.tarasevich@gmail.com>
2023-03-19 18:15:24 +0100

    Change local dir name

--- installation/let_jovyan_write.sh
+++ installation/let_jovyan_write.sh
@@ -1,3 +1,3 @@
 # script that provides jovyan, the default Docker container username, permission to write to the directory
-sudo chgrp -R 100 DLTFpT/
-sudo chmod -R g+w DLTFpT/ #to recursively change permissions so jovyan can write to the directory
+sudo chgrp -R 100 DL_illustrated_TF2/
+sudo chmod -R g+w DL_illustrated_TF2/ #to recursively change permissions so jovyan can write to the directory
