6a2d064b5f1dc48c67fee5436fb0acbc293fa277
vadikch <v.v.tarasevich@gmail.com>
2023-03-19 19:16:00 +0100

    right docker image path

--- rundocker.sh
+++ rundocker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo docker run -v $(pwd):/home/jovyan/work -it --rm -p 8888:8888 dltfpt-stack
+#sudo docker run -v $(pwd):/home/jovyan/work -it --rm -p 8888:8888 dltfpt-stack
+sudo docker run -v $(pwd):/home/jovyan/work -it --rm -p 8888:8888 jonkrohn/dltfpt-stack:videos
